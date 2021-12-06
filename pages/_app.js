2790e5e4405fd17843be782d1b68b77ece5bf6b3
Alex <kuzminbass@gmail.com>
2021-12-06 11:13:09 +1000

    pageProps

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,27 @@
 import Page from "../components/Page";
+import { ApolloProvider } from "@apollo/client";
+import withData from "../lib/withData";
 
-export default function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, apollo }) {
   return (
-    <Page>
-      <Component {...pageProps} />
-    </Page>
+    <ApolloProvider client={apollo}>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </ApolloProvider>
   );
 }
+
+MyApp.getInitialProps = async function ({ Component, ctx }) {
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+
+  pageProps.query = ctx.query;
+  console.log("line 23", pageProps.query);
+  return { pageProps };
+};
+export default withData(MyApp);
+
+// https://griko.medium.com/exploring-undocumented-getinitialprops-properties-on-next-js-1265a6abc652
