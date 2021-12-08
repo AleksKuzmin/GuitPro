edaf9d19cf0806806725830fb0dde9093924d718
Alex <kuzminbass@gmail.com>
2021-12-08 14:40:04 +1000

    sign up

--- pages/signin.js
+++ pages/signin.js
@@ -1,10 +1,19 @@
+import styled from "styled-components";
+
 import SignIn from "../components/SignIn";
 import SignUp from "../components/SignUp";
+
+const GridStyles = styled.div`
+  display: grid;
+  grid-template-colums: repeat(auto, minmax(300px, 1fr));
+  grid-gap: 2rem;
+`;
+
 export default function SignInPage() {
   return (
-    <div>
+    <GridStyles>
       <SignIn />
       <SignUp />
-    </div>
+    </GridStyles>
   );
 }
